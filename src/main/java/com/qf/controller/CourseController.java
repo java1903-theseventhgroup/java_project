65fdbcbf49399711b57e5cf09401c12c3d183ca4
huodengpan <1023657575@qq.com>
2020-05-23 15:30:37 +0800

    huo-增加了删除接口

--- src/main/java/com/qf/controller/CourseController.java
+++ src/main/java/com/qf/controller/CourseController.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
-
+//xiugai个啥呀
 @Controller
 @ResponseBody
 @RequestMapping("Course")
